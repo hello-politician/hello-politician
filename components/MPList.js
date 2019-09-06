203d56f8d324fd6a150bb41a13cd7542e944356f
David Brown <david.brown@windowslive.com>
2019-09-07 07:34:05 +1200

    MPList creatred

---
+++ components/MPList.js
@@ -0,0 +1,40 @@
+import { Container } from 'react-bootstrap'
+
+const listMPs = [
+  {
+    id: 1,
+    name: 'Jacinda Ardern',
+    electorate: 'Mount Albert',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ardern_Cropped.png/220px-Ardern_Cropped.png'
+  },
+  {
+    id: 2,
+    name: 'Jacinda Ardern',
+    electorate: 'Mount Albert',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ardern_Cropped.png/220px-Ardern_Cropped.png'
+  },
+  {
+    id: 3,
+    name: 'Jacinda Ardern',
+    electorate: 'Mount Albert',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ardern_Cropped.png/220px-Ardern_Cropped.png'
+  },
+  {
+    id: 4,
+    name: 'Jacinda Ardern',
+    electorate: 'Mount Albert',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ardern_Cropped.png/220px-Ardern_Cropped.png'
+  }
+]
+
+const MPList = ({ mpList }) => (
+  <Container fluid>
+    {mpList && mpList.map(mp => <MPCard key={mp.id} mp={mp} />)}
+  </Container>
+)
+
+export default MPList
