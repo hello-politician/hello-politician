b2b52b89db8808f8f443d36a7cc5c1f24f0b321d
David Brown <davidbrown@wifi-guest-172-24-109-206.net.auckland.ac.nz>
2019-09-07 10:54:22 +1200

    list and card done

--- components/MPList.js
+++ components/MPList.js
@@ -1,40 +1,46 @@
-import { Container } from 'react-bootstrap'
+import MPCard from "./MPCard";
+import styled from "styled-components";
 
 const listMPs = [
   {
     id: 1,
-    name: 'Jacinda Ardern',
-    electorate: 'Mount Albert',
+    name: "Jacinda Ardern",
+    electorate: "Mount Albert",
     image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ardern_Cropped.png/220px-Ardern_Cropped.png'
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ardern_Cropped.png/220px-Ardern_Cropped.png"
   },
   {
     id: 2,
-    name: 'Jacinda Ardern',
-    electorate: 'Mount Albert',
+    name: "Jacinda Ardern",
+    electorate: "Mount Albert",
     image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ardern_Cropped.png/220px-Ardern_Cropped.png'
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ardern_Cropped.png/220px-Ardern_Cropped.png"
   },
   {
     id: 3,
-    name: 'Jacinda Ardern',
-    electorate: 'Mount Albert',
+    name: "Jacinda Ardern",
+    electorate: "Mount Albert",
     image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ardern_Cropped.png/220px-Ardern_Cropped.png'
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ardern_Cropped.png/220px-Ardern_Cropped.png"
   },
   {
     id: 4,
-    name: 'Jacinda Ardern',
-    electorate: 'Mount Albert',
+    name: "Jacinda Ardern",
+    electorate: "Mount Albert",
     image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ardern_Cropped.png/220px-Ardern_Cropped.png'
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ardern_Cropped.png/220px-Ardern_Cropped.png"
   }
-]
+];
+
+const Container = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
 
 const MPList = ({ mpList }) => (
   <Container fluid>
-    {mpList && mpList.map(mp => <MPCard key={mp.id} mp={mp} />)}
+    {listMPs && listMPs.map(mp => <MPCard key={mp.id} mp={mp} />)}
   </Container>
-)
+);
 
-export default MPList
+export default MPList;
