e9f7e6fe435651b94c8ea69d2c248c99b331f41b
morley-stuff <morleyj.mail@gmail.com>
2019-09-08 00:11:26 +1200

    Introduced candidate donations and expenses data implements script to combine with base data set adds script execution to data pipeline

--- data/data-pipeline.sh
+++ data/data-pipeline.sh
@@ -1,5 +1,6 @@
 cp members-of-parliament/members-of-parliament.json target/temp.json
 groovy disclosure-of-member-expenses/src/update-member-disclosure.groovy members-of-parliament/members-of-parliament.json disclosure-of-member-expenses/data/format-new/ target/temp.json new 
 groovy disclosure-of-member-expenses/src/update-member-disclosure.groovy target/temp.json disclosure-of-member-expenses/data/format-old/ target/temp.json old 
+groovy candidate-donations-expenses/src/add-candidate-donations-expenses.groovy target/temp.json candidate-donations-expenses/data/2017-nz-general-election-candidates-returns_donations-and-expenses.json target/temp.json >target/output.log
 mv target/temp.json target/output.json
 echo finished
