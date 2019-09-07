d42ad0523e4b74d326433d6540ba1fcd00e8e5ed
Minh-Phu <Lminhphu0304@gmail.com>
2019-09-07 13:48:48 +1200

    Update member of paliament.json data

--- data/process.py
+++ data/process.py
@@ -1,2 +1,14 @@
-import json 
-import
+import json
+
+catagory = [ '', 'Company directorships and controlling interests', 'Interests', 'Employment', 'Interests in trusts', 'Organisations and trusts seeking Government funding','Real property','Superannuation schemes','Managed investment schemes','Debtors','Creditors','Overseas travel','Gifts','Discharged debts','Payments for activities']
+
+data = []
+with open('memberinfo.json') as data_file:    
+    data.append(json.load(data_file))
+
+for i in range(0,117):
+    fileStream = open('MP-%i.txt'%(i),'r')
+    content = fileStream.read()
+    
+    fileStream.close()
+    # for line in content:
