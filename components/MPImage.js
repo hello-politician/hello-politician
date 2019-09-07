5163b8267fe384d86d54d4be76d8bc9496d63b35
Aidan Harris <aidanharris1001@gmail.com>
2019-09-07 16:11:43 +1200

    We've got some images loading!!

--- components/MPImage.js
+++ components/MPImage.js
@@ -1,18 +1,23 @@
 import React from 'react'
-import Card from "react-bootstrap/Card";
+import Card from 'react-bootstrap/Card'
+const images = require.context("../images/parliament/")
+
 export default class MPImage extends React.Component{
   calculateImageName() {
-    return "./../images/Parliament/" + (this.props.mpName ? this.props.mpName.replace(",", "").replace(" ", "") + ".jpg" : "AdamsAmy.jpg");
+    let imageName = (this.props.mpName ? ("./" + this.props.mpName.replace(",", "").replace(/ /g, "") + ".jpg") : "");
+    if(images.keys().includes(imageName))
+      return images(imageName)
+    
+    return images('./placeholder.png')
   }
-  // AdamsAmy.jpg
-  // "AdamsAmy.jpg"
 
   constructor() {
     super()
     this.calculateImageName = this.calculateImageName.bind(this)
   }
-//+ this.calculateImageName()
+
   render() {
-    return <Card.Img src={require("../images/Parliament/AdamsAmy.jpg" )}></Card.Img>
+    return <Card.Img src={(this.calculateImageName())}/>
+    
   }
 }
--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 // next.config.js
 const withSass = require('@zeit/next-sass')
-module.exports = withSass({
+const withImages = require('next-images')
+
+module.exports = withImages(withSass({
   /* config options here */
-})
+}))
