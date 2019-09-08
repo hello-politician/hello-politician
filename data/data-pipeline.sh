41b889cc420bf451c3aa118bb9c0846b8d973b36
jmorley <jmorley@bravurasolutions.com>
2019-09-08 13:56:17 +1200

    Added individual donation data added script to incorporate donation data into whole added script to data pipeline

--- data/data-pipeline.sh
+++ data/data-pipeline.sh
@@ -1,7 +1,8 @@
 python financial/process.py members-of-parliament/members-of-parliament.json financial/ target/temp.json
-groovy disclosure-of-member-expenses/src/update-member-disclosure.groovy target/temp.json disclosure-of-member-expenses/data/format-new/ target/temp.json new >target/output.log 
-groovy disclosure-of-member-expenses/src/update-member-disclosure.groovy target/temp.json disclosure-of-member-expenses/data/format-old/ target/temp.json old >target/output.log
-groovy candidate-donations-expenses/src/add-candidate-donations-expenses.groovy target/temp.json candidate-donations-expenses/data/2017-nz-general-election-candidates-returns_donations-and-expenses.json target/temp.json >target/output.log
-groovy members-tenure/src/add-members-tenure.groovy target/temp.json members-tenure/data/tenure.json target/temp.json
+groovy disclosure-of-member-expenses/src/update-member-disclosure.groovy target/temp.json disclosure-of-member-expenses/data/format-new/ target/temp.json new >>target/output.log 
+groovy disclosure-of-member-expenses/src/update-member-disclosure.groovy target/temp.json disclosure-of-member-expenses/data/format-old/ target/temp.json old >>target/output.log
+groovy candidate-donations-expenses/src/add-candidate-donations-expenses.groovy target/temp.json candidate-donations-expenses/data/2017-nz-general-election-candidates-returns_donations-and-expenses.json target/temp.json >>target/output.log
+groovy members-tenure/src/add-members-tenure.groovy target/temp.json members-tenure/data/tenure.json target/temp.json >> target/output.log
+groovy candidate-donations-expenses/src/add-individual-donations.groovy target/temp.json candidate-donations-expenses/data/2017-general-election-candidates-individual-donors.json target/temp.json >> target/output.log
 mv target/temp.json target/output.json
 echo finished
